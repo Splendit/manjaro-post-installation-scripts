73f7c14118615fe014667266e2c60a37f28087d2
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2019-03-06 10:20:29 +0100

    ADD meld to the optionals

---
+++ optional/090_meld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "======================================================================"
+echo "install meld (graphical diff view)"
+echo "======================================================================"
+
+# install meld
+yay --verbose -S --noconfirm --needed meld
